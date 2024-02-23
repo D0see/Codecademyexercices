71062c38aa9b563358a30adeffe4ae76a337e283
D0see <317Y17@gmail.com>
2024-02-23 01:12:06 +0100

    Exo 2
    Pretty happy with my solution for the final problem :dog:

---
+++ Js/exo2/main.js
@@ -0,0 +1,113 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ['A', 'T', 'C', 'G'];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
+
+idCount = 0;
+function pAequorFactory(specimenNum = idCount, dna = mockUpStrand()) {
+  idCount++;
+  return {
+    specimenNum: idCount,
+    dna: mockUpStrand(),
+    mutate() {
+      const randomDnaBaseIndex = Math.floor(Math.random()*this.dna.length);
+      let newRandomBase;
+      do {
+        newRandomBase = returnRandBase()
+      } while (this.dna[randomDnaBaseIndex] === newRandomBase) 
+      this.dna[randomDnaBaseIndex] = newRandomBase;
+    },
+    compareDna(obj) {
+      const totalBase = this.dna.length;
+      let dnaMatches = 0;
+      let matchPercent;
+      for (let i = 0; i < this.dna.length; i++) {
+        if (obj.dna[i] === this.dna[i]) {
+          dnaMatches++;
+        }
+      }
+      matchPercent = Math.ceil((dnaMatches/totalBase)*100);
+      console.log(`Specimen ${obj.specimenNum} and specimen ${this.specimenNum} have ${matchPercent}% of dna in common`); 
+      return matchPercent;
+    },
+    willLikelySurvive() {
+      const totalBase = this.dna.length;
+      let numOfCAndG = 0;
+      for (const base of this.dna) {
+        if (base === 'C' || base === 'G') {
+          numOfCAndG++;
+        }
+      }
+      let cAndGPercent = (numOfCAndG/totalBase)*100; 
+      return (cAndGPercent > 60);
+    },
+    complementStrand() {
+      let complementaryStrand = [];
+      for (const base of this.dna) {
+        switch (base) {
+          case 'A':
+            complementaryStrand.push('T');
+            break;
+          case 'T':
+            complementaryStrand.push('A');
+            break;
+          case 'C':
+            complementaryStrand.push('G');
+            break;
+          case 'G':
+            complementaryStrand.push('C');
+            break;
+        }
+      }
+      return complementaryStrand;
+    }
+  }
+  
+}
+
+let storage = []
+
+while (storage.length < 30) {
+  let newCreature = pAequorFactory();
+  while(!newCreature.willLikelySurvive()) {
+    newCreature.mutate();
+  }
+  storage.push(newCreature);
+}
+
+console.log(storage);
+console.log(storage[29].complementStrand());
+
+function findMostRelated(arr) {
+  let bestScore = 0;
+  let score;
+  let mostRelated1;
+  let mostRelated2;
+  for (let j = 0; j < arr.length; j++) {
+    for (let i = 0 + j; i < arr.length; i++) {
+      if (i === j) {
+        continue;
+      } else {
+        score = arr[i].compareDna(arr[j]);
+        if (score > bestScore) {
+          mostRelated1 = arr[i];
+          mostRelated2 = arr[j];
+          bestScore = score;
+        }
+      }
+    }
+  }
+  console.log(`the 2 closest specimens are ${mostRelated1.specimenNum} and ${mostRelated2.specimenNum} with a score of ${bestScore}%`);
+}
+
+findMostRelated(storage);
